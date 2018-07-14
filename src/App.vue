58fa1ac426c55ed02ea9861163519207a13ae452
Alex <alex.weber312@gmail.com>
2018-07-14 17:47:25 +0200

    *base

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view name="navbar"></router-view>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "sass/main";
+</style>
